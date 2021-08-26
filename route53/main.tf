9e61874b78125edcb4ef67e09802d56ab408a9e7
umihico <umihico@users.noreply.github.com>
2021-08-27 07:17:33 +0900

    feat: vertify umihico.com as google secondary domain

--- route53/main.tf
+++ route53/main.tf
@@ -1,4 +1,15 @@
+locals {
+  domain = "umihico.com"
+}
 resource "aws_route53_zone" "umihico_com" {
   comment = "HostedZone created by Route53 Registrar" # and imported
-  name    = "umihico.com"
+  name    = local.domain
+}
+
+resource "aws_route53_record" "record_txt" {
+  zone_id = aws_route53_zone.umihico_com.zone_id
+  name    = local.domain
+  type    = "TXT"
+  ttl     = "300"
+  records = [var.secret_google_txt]
 }
