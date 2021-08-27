671f88b05627aa5a9cecd275c436a958c2ce9d01
umihico <umihico@users.noreply.github.com>
2021-08-28 08:37:30 +0900

    feat: add gmail mx records

--- route53/main.tf
+++ route53/main.tf
@@ -13,3 +13,17 @@ resource "aws_route53_record" "gsuite_domain_vertification" {
   ttl     = "300"
   records = [var.secret_google_txt]
 }
+
+resource "aws_route53_record" "gsuite_gmail_setup" {
+  zone_id = aws_route53_zone.umihico_com.zone_id
+  name    = local.domain
+  type    = "MX"
+  ttl     = 300
+  records = [
+    "1 aspmx.l.google.com",
+    "5 alt1.aspmx.l.google.com",
+    "5 alt2.aspmx.l.google.com",
+    "10 alt3.aspmx.l.google.com",
+    "10 alt4.aspmx.l.google.com"
+  ]
+}
